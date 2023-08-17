e6f78f1998e14b56719c9771dda332a072a97d2e
toannguyen7202 <toannguyen7202@gmail.com>
2023-08-17 14:39:18 +0700

    cloud dinary config

---
+++ src/main/java/vn/iotstar/student_helpdesk_hcmute_be/config/CloudinaryConfig.java
@@ -0,0 +1,28 @@
+package vn.iotstar.student_helpdesk_hcmute_be.config;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfig {
+
+    @Value("${cloudinary.cloud-name}")
+    private String CloudName;
+
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary(){
+        Cloudinary cloudinary = new Cloudinary("cloudinary://"+this.apiKey+":"+this.apiSecret+"@"+this.CloudName);
+        return cloudinary;
+    }
+}
+
+
+
