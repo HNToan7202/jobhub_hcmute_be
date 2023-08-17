6890dffaed9b1dfb12fcf6a9e9c1b899cb2fc7df
toannguyen7202 <toannguyen7202@gmail.com>
2023-08-17 13:11:46 +0700

    create project

---
+++ src/test/java/vn/iotstar/student_helpdesk_hcmute_be/StudentHelpdeskHcmuteBeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.iotstar.student_helpdesk_hcmute_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHelpdeskHcmuteBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
