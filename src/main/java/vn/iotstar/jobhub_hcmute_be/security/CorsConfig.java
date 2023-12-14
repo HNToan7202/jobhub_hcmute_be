3353ff9d60d878b441e7460658a8678efb883e1c
CongThanh27 <nguyencongthanh280702@gmail.com>
2023-12-14 17:43:18 +0700

    CorsConfig

--- src/main/java/vn/iotstar/jobhub_hcmute_be/security/CorsConfig.java
+++ src/main/java/vn/iotstar/jobhub_hcmute_be/security/CorsConfig.java
@@ -1,18 +1,20 @@
-package vn.iotstar.jobhub_hcmute_be.security;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true);
-    }
-}
+//package vn.iotstar.jobhub_hcmute_be.security;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableWebMvc
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+////                .allowedOrigins("*")
+//                .allowedOriginPatterns("*")
+//                .allowedHeaders("*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowCredentials(false);
+//    }
+//}
